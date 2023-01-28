bf0fbe9889893d452c78ac3122aa1018f281a345
pradeep-pulaparthi <pradeeppulaparthi99@gmail.com>
2023-01-28 11:59:19 +0530

    added color to base.py

--- base.py
+++ base.py
@@ -1,6 +1,9 @@
 import turtle
 #printing a star
+turtle.begin_fill()
+turtle.pencolor("blue")
 for i in range(5):
-    turtle.forward(40)
-    turtle.left(72)
+    turtle.forward(140)
+    turtle.left(144)
+turtle.end_fill()
 turtle.done()
